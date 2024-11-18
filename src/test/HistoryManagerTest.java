65733aa293acb6c6108518e548b9ee14207b61b6
baronov24 <barosha24@yandex.ru>
2024-11-18 12:42:55 +0300

    После предыдущего исправления не проходил тест HistoryManagerTest, исправлено.

--- src/test/HistoryManagerTest.java
+++ src/test/HistoryManagerTest.java
@@ -1,7 +1,6 @@
 package test;
 
 import managers.InMemoryTaskManager;
-import managers.Managers;
 import managers.TaskManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -15,10 +14,10 @@ public class HistoryManagerTest {
         Task task = new Task(1, "Сходить в школу", "Получить 5-ку");
         manager.newTask(task);
 
-        Assertions.assertEquals(0, Managers.getDefaultHistory().getHistory().size());
+        Assertions.assertEquals(0, manager.getHistory().size());
 
-        manager.getSubtask(1);
+        manager.getTask(1);
 
-        Assertions.assertEquals(1, Managers.getDefaultHistory().getHistory().size());
+        Assertions.assertEquals(1, manager.getHistory().size());
     }
 }
