cdfca48342e5a8d5535d39bebe27e5d57f666bfd
baronov24 <barosha24@yandex.ru>
2025-01-12 18:23:29 +0300

    Класс HistoryList перенесен внутрь класса InMemoryHistoryManager

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -9,6 +9,63 @@ import java.util.List;
 import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
+    static class HistoryList {
+        Map<Integer, Node> map = new HashMap<>();
+        private Task head;
+        private Task tail;
+
+        void linkLast(Task task) {
+            Node node;
+
+            if (map.containsKey(task.getId())) {
+                removeNode(map.get(task.getId()));
+            }
+
+            if (map.isEmpty()) {
+                head = task;
+                tail = task;
+                node = new Node(task, null);
+            } else {
+                map.get(tail.getId()).setNextTask(task);
+                node = new Node(task, tail);
+                tail = task;
+            }
+
+            map.put(task.getId(), node);
+        }
+
+        void removeNode(Node node) {
+            map.remove(node.getTask().getId());
+
+            Task prevTask = node.getPrevTask();
+            Task nextTask = node.getNextTask();
+
+            if (prevTask == null) {
+                head = nextTask;
+            } else {
+                map.get(prevTask.getId()).setNextTask(nextTask);
+            }
+
+            if (nextTask == null) {
+                tail = prevTask;
+            } else {
+                map.get(nextTask.getId()).setPrevTask(prevTask);
+            }
+        }
+
+        public List<Task> getTasks() {
+            List<Task> answer = new ArrayList<>();
+            Task task = head;
+
+            while (task != null) {
+                answer.add(task);
+                task = map.get(task.getId()).getNextTask();
+            }
+
+            return answer;
+        }
+    }
+
     HistoryList history = new HistoryList();
 
     @Override
@@ -27,62 +84,5 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     public void remove(int id) {
         history.removeNode(history.map.get(id));
-    }
-}
-
-class HistoryList {
-    Map<Integer, Node> map = new HashMap<>();
-    private Task head;
-    private Task tail;
-
-    void linkLast(Task task) {
-        Node node;
-
-        if (map.containsKey(task.getId())) {
-            removeNode(map.get(task.getId()));
-        }
-
-        if (map.isEmpty()) {
-            head = task;
-            tail = task;
-            node = new Node(task, null);
-        } else {
-            map.get(tail.getId()).setNextTask(task);
-            node = new Node(task, tail);
-            tail = task;
-        }
-
-        map.put(task.getId(), node);
-    }
-
-    void removeNode(Node node) {
-        map.remove(node.getTask().getId());
-
-        Task prevTask = node.getPrevTask();
-        Task nextTask = node.getNextTask();
-
-        if (prevTask == null) {
-            head = nextTask;
-        } else {
-            map.get(prevTask.getId()).setNextTask(nextTask);
-        }
-
-        if (nextTask == null) {
-            tail = prevTask;
-        } else {
-            map.get(nextTask.getId()).setPrevTask(prevTask);
-        }
-    }
-
-    public List<Task> getTasks() {
-        List<Task> answer = new ArrayList<>();
-        Task task = head;
-
-        while (task != null) {
-            answer.add(task);
-            task = map.get(task.getId()).getNextTask();
-        }
-
-        return answer;
     }
 }
